e676f3193b8773d9e707c069839ca1fcba6145fb
Hitarth Joshi <joshihitarth07@gmail.com>
2026-09-07 14:21:41 +0530

    implement matches POST and GET routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import express from "express"
+import { matchRouter } from "./routes/matches.js";
 
 const app = express();
 const port = process.env.port || 8000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("hello message from server")
 })
+
+app.use('/matches', matchRouter)
 
 app.listen(port, () => {
     console.log(`App is listinf on the following port ${port} and server listen on http://localhost:${port}`)
